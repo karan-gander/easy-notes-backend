5afa186cea680d90d1bdb29a0744d2787782b4bf
karan-gander <meghawalkaran@gmail.com>
2024-07-26 10:24:14 +0530

    minor update tring 1

--- src/controllers/user/userLogin.js
+++ src/controllers/user/userLogin.js
@@ -38,7 +38,7 @@ const userLogin = asyncHandler(async (req, res) => {
   };
   return res
     .status(200)
-    .cookie("accessToken", accessToken,options)
+    .cookie("accessToken", accessToken)
     .json(
       new ApiResponse(
         200,
